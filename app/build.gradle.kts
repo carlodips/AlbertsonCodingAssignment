86b11219e0e1ada3606879a99afcb98b38c6359b
carlodips <ccdipasupil@gmail.com>
2024-10-09 00:35:48 +0800

    Fix compiling error due to Kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,10 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
@@ -62,7 +63,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -70,7 +71,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- app/src/main/java/dev/carlodips/albertsoncodingassignment/RandomUsersApplication.kt
+++ app/src/main/java/dev/carlodips/albertsoncodingassignment/RandomUsersApplication.kt
@@ -3,6 +3,5 @@ package dev.carlodips.albertsoncodingassignment
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-// To initiate the Dagger Hilt module on start of the application.
 @HiltAndroidApp
-class RandomUsersApp: Application()
+class RandomUsersApplication: Application()
