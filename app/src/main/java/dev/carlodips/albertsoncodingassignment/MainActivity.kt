7e3d9d47b932758438314fe0a83f8240eb67d931
carlodips <ccdipasupil@gmail.com>
2024-10-10 17:34:52 +0800

    Added back button in app bar and animation when navigating

--- app/src/main/java/dev/carlodips/albertsoncodingassignment/MainActivity.kt
+++ app/src/main/java/dev/carlodips/albertsoncodingassignment/MainActivity.kt
@@ -2,6 +2,8 @@ package dev.carlodips.albertsoncodingassignment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import dev.carlodips.albertsoncodingassignment.databinding.ActivityMainBinding
@@ -10,6 +12,11 @@ import dev.carlodips.albertsoncodingassignment.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var hasBackStackListener: Boolean = false
+
+    private val childFragmentManager: FragmentManager?
+        get() = supportFragmentManager.primaryNavigationFragment?.childFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +30,49 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = applicationContext.getString(R.string.app_name)
     }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        if (hasBackStackListener) return
+
+        val fm = childFragmentManager ?: return
+        fm.addOnBackStackChangedListener {
+            if (fm.backStackEntryCount > 0)
+                addBackButtonInToolbar()
+            else
+                removeBackButtonInToolbar()
+        }
+
+        if (fm.backStackEntryCount > 0 && binding.appbar.navigationIcon == null)
+            addBackButtonInToolbar()
+
+        hasBackStackListener = true
+    }
+
     private fun setStartingNavGraph() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(binding.mainContent.id) as NavHostFragment
         val navController = navHostFragment.navController
 
         navController.graph = navController.navInflater.inflate(R.navigation.nav_root)
+    }
+
+    private fun addBackButtonInToolbar() {
+        with(binding.appbar) {
+            navigationIcon = AppCompatResources.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_arrow_back
+            )
+
+            setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
+    }
+    private fun removeBackButtonInToolbar() {
+        with(binding.appbar) {
+            navigationIcon = null
+            setNavigationOnClickListener(null)
+        }
     }
 }
